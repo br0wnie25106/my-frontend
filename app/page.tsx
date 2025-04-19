80bae824aa244b1e8fed847b68860fc0929f7d90
pitchaya <106579110+br0wnie25106@users.noreply.github.com>
2025-04-19 17:25:30 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 // app/page.tsx
+import { BlackjackSimulator } from "@/components/BlackjackSimulator";
+
+
 export default function Home() {
-  return (
-    <main className="min-h-screen p-8">
-      <h1>Hello from Blackjack Frontend 👋</h1>
-    </main>
-  );
+  return <BlackjackSimulator />;
 }
+
